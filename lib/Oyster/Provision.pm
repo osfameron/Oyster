3ded63474d7f715bb3c6be48ad6c39cb63f726df
iain <iainhubbard@gmail.com>
2010-11-20 16:00:59 +0000

    added basic provision files

--- lib/Oyster/Provision.pm
+++ lib/Oyster/Provision.pm
@@ -1,3 +1,18 @@
-package Oyster::Provision
+package Oyster::Provision;
+
+use Moose;
+
+sub config {
+    return {provision_backend => 'Oyster::Provision::Rackspace'};
+}
+
+sub BUILD {
+
+    my $self = shift;
+
+    my $role = $self->config()->{provision_backend};
+
+    "$role"->meta->apply($self);
+}
 
 1;
--- lib/Oyster/Provision/Rackspace.pm
+++ lib/Oyster/Provision/Rackspace.pm
@@ -1,3 +1,25 @@
 package Oyster::Provision::Rackspace;
+use Moose::Role;
+
+requires 'config';
+
+sub create {
+   my $self = shift;
+
+   $self->config();
+}
+
+
+sub delete {
+   my $self = shift;
+
+   $self->config();
+}
+
+sub resize {
+   my $self = shift;
+
+   $self->config();
+}
 
 1;
