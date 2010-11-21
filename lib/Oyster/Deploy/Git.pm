c0ce95cc18f039ddbd57762ffe7e0581f9ecfcad
Marco Fontani <MFONTANI@cpan.org>
2010-11-21 23:24:36 +0000

    Too much Whitespace

--- lib/Oyster/Deploy/Git.pm
+++ lib/Oyster/Deploy/Git.pm
@@ -11,17 +11,17 @@ use File::ShareDir ':ALL';
 sub create {
   my $self = shift;
   my $location = shift;
-  
+
   if( -f $location || -d $location ) {
     Error::Simple->throw("$location already exists");
   }
-  
+
   mkdir($location);
   my $git = Git::Wrapper->new($location);
   $git->init();
-  
+
   my ($postreceive, $postupdate);
-  
+
   eval {
     $postreceive = dist_file( 'Oyster', './deploy/git/post-receive');
     $postupdate = dist_file( 'Oyster', './deploy/git/post-update');
@@ -35,12 +35,12 @@ sub create {
 
   copy($postreceive, ($git->dir . '/.git/hooks/')) 
     or Error::Simple->throw('Creating post commit hooks failed.');
-    
+
   copy($postupdate, ($git->dir . '/.git/hooks/')) 
     or Error::Simple->throw('Creating post commit hooks failed.');
-  
+
   chmod(0x755, ($git->dir . '.git/hooks/post-receive', $git->dir . '.git/hooks/post-update'));
-  
+
   return 1;
 }
 
